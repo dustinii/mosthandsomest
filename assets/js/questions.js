c30932e3f40aefc782874f53e674135a6314edee
dustinii <125335987+dustinii@users.noreply.github.com>
2023-07-10 19:32:47 -0700

    filled index.html, added questions array

---
+++ assets/js/questions.js
@@ -0,0 +1,77 @@
+var questions = [
+    {
+        title: 'Which of the following is not a reserved word in JavaScript?',
+        choices: ['interface', 'throws', 'program', 'short'],
+        answer: 'program'
+    },
+    {
+        title: 'How can a value be appended to an array?',
+        choices: ['arr(length) = value', 'arr[arr.length] = value', 'arr = arr + value', 'None of the above'],
+        answer: 'arr[arr.length] = value'
+    },
+    {
+        title: 'What will the code return? Boolean(3 < 7)',
+        choices: ['true', 'false', 'NaN', 'SyntaxError'],
+        answer: 'true'
+    },
+    {
+        title: 'How does JavaScript store dates in objects of Date type?',
+        choices: ['The number of days since January 1, 1900', 'The number of milliseconds since January 1, 1970', 'The number of milliseconds since January 1, 2000', 'The number of seconds since Netscape’s public stock offering.'],
+        answer: 'The number of milliseconds since January 1, 1970'
+    },
+    {
+        title: 'Which of the following is not considered a JavaScript operator?',
+        choices: ['new', 'this', 'delete', 'typeof'],
+        answer: 'this'
+    },
+    {
+        title: 'Which of the following is not a valid JavaScript variable name?',
+        choices: ['2names', '_first_and_last_names', 'FirstAndLast', 'None of the above'],
+        answer: '2names'
+    },
+    {
+        title: 'In JavaScript, which of the following is not a logical operator?',
+        choices: ['!', '&&', '&', '||'],
+        answer: '&'
+    },
+    {
+        title: 'Which of the following looping structures is/are valid in JavaScript?',
+        choices: ['for', 'while', 'do-while', 'All of the above'],
+        answer: 'All of the above'
+    },
+    {
+        title: 'What does the following JavaScript code do? contains(arr, obj)',
+        choices: ['It checks if arr contains obj', 'It finds obj in array arr and returns its index', 'It iterates through array arr and returns a new array with all elements lower than obj', 'It reduces arr to a single value by repetitively calling obj'],
+        answer: 'It checks if arr contains obj'
+    },
+    {
+        title: 'What does “1”+2+4 evaluate to?',
+        choices: ['124', '7', 'NaN', 'None of the above'],
+        answer: '124'
+    },
+    {
+        title: 'What will be the output of the following JavaScript code? <br><br>var x = 1; <br>if (function f(){}) { <br> x += typeof f; <br> } <br> console.log(x);',
+        choices: ['1undefined', '1function', 'NaN', 'undefined'],
+        answer: '1undefined'
+    },
+    {
+        title: 'Which JavaScript label catches all the values, except for the ones specified?',
+        choices: ['catch', 'label', 'try', 'default'],
+        answer: 'default'
+    },
+    {
+        title: 'What will be the output of the following JavaScript code? <br><br>var x = 5; <br>console.log(x === 5 ? "true" : "false");',
+        choices: ['true', 'false', 'NaN', 'TypeError'],
+        answer: 'true'
+    },
+    {
+        title: 'What will be the output of the following JavaScript code? <br><br>console.log(null == undefined);',
+        choices: ['true', 'false', 'null', 'undefined'],
+        answer: 'true'
+    },
+    {
+        title: 'What will be the output of the following JavaScript code? <br><br>console.log(0.1 + 0.2 == 0.3);',
+        choices: ['true', 'false', 'NaN', 'undefined'],
+        answer: 'false'
+    },
+];
